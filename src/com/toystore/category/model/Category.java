7afcaa47c0f5f906c8e2a555bd66ad5b91115edb
IT25100368 <it25100368@my.sliit.lk>
2026-04-19 23:22:37 +0530

    Added category model class and service class

---
+++ src/com/toystore/category/model/Category.java
@@ -0,0 +1,42 @@
+package com.toystore.category.model;
+
+public class Category {
+    private String id;
+    private String name;
+    private String description;
+
+    // Constructor
+    public Category(String id, String name, String description) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+    }
+
+    // Getters
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    // Convert object to file format
+    public String toFileString() {
+        return id + "," + name + "," + description;
+    }
+}
+}
